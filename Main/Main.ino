69d8c3c20bd7707278d987a9b72b796a68744196
Jordintown <jordibeltrandonate@gmail.com>
2024-09-08 19:09:55 +0200

    Bad hardware attempt

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,19 +1,21 @@
 
 unsigned long clock,segundos,ultimoclock,segundos1,segundos2;
 
-
-int jugador1=0;
-
-
 void setup(){
   Serial.begin(9600);
   ultimoclock=0;
-  segundos1=0;
-  segundos2=0;
-  
+  segundos1=500;
+  segundos2=500;
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+
 }
 
 void loop(){
+  int moves1=0;
+  int moves2=0;
+  int jugador1;
+  
   clock = millis();
   segundos = clock/1000;
   if (Serial.available() > 0) {
@@ -22,9 +24,27 @@ void loop(){
   }
   if(clock>(ultimoclock+999)){
     ultimoclock=clock;
-    Serial.print(String(segundos)+" | "+String(segundos));
-    if(jugador1) Serial.println(" <");
-    else Serial.println(" >");
+    
+    if(jugador1=0){
+      segundos1--;
+      moves1++;
+      Serial.println(String(segundos1)+" < "+String(segundos2))+"\n";
+      digitalWrite(11, HIGH);
+      digitalWrite(12, LOW);  
+        if (segundos1<1);{
+
+      }
+    } 
+    else{
+      segundos2--;
+      moves2++;
+      Serial.println(String(segundos1)+" > "+String(segundos2))+"\n";
+      digitalWrite(12, HIGH);
+      digitalWrite(11, LOW);  
+      if (segundos2<1);{
+
+      }
+    } 
   }
 
 } 
