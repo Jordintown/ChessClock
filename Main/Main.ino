a1f6eb945e888ec97a628fa3a61d5bb18574cf5b
Jordintown <jordibeltrandonate@gmail.com>
2024-09-08 17:57:58 +0200

    skeletyon

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,11 +1,30 @@
-//DME Chess Clock for Arduino
-//Version: 0.0.0A
-//Use under GNU GPL 3.0
+
+unsigned long clock,segundos,ultimoclock,segundos1,segundos2;
+
+
+int jugador1=0;
+
 
 void setup(){
-Serial.print("test");
+  Serial.begin(9600);
+  ultimoclock=0;
+  segundos1=0;
+  segundos2=0;
+  
 }
 
 void loop(){
-    
+  clock = millis();
+  segundos = clock/1000;
+  if (Serial.available() > 0) {
+        Serial.print((char)Serial.read());
+        jugador1=!jugador1;
+  }
+  if(clock>(ultimoclock+999)){
+    ultimoclock=clock;
+    Serial.print(String(segundos)+" | "+String(segundos));
+    if(jugador1) Serial.println(" <");
+    else Serial.println(" >");
+  }
+
 } 
